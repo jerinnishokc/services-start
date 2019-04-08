8c5e7367f4aa0edf8791e9e0d5fa935cc292faea
jerinnishokc <jerinnishok@outlook.com>
2019-04-08 21:00:57 +0530

    Added Data service - AccountsService

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
 
 @Component({
   selector: 'app-account',
@@ -12,10 +13,11 @@ export class AccountComponent {
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor(private loggingService: LoggingService){}
+  constructor(private loggingService: LoggingService, private accountsService: AccountsService){}
 
   onSetTo(status: string) {
-    this.statusChanged.emit({id: this.id, newStatus: status});
+    //this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateStatus(this.id, status);
     this.loggingService.logStatusChange(status);
     //console.log('A server status changed, new status: ' + status);
   }
