a7b43d0ce29a3c7a774d29bf2ead607955c6af33
jerinnishokc <jerinnishok@outlook.com>
2019-04-08 21:36:15 +0530

    Injecting service inside another service

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { LoggingService } from '../logging.service';
 import { AccountsService } from '../accounts.service';
 
@@ -11,14 +11,10 @@ import { AccountsService } from '../accounts.service';
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
-  @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
   constructor(private loggingService: LoggingService, private accountsService: AccountsService){}
 
   onSetTo(status: string) {
-    //this.statusChanged.emit({id: this.id, newStatus: status});
     this.accountsService.updateStatus(this.id, status);
-    this.loggingService.logStatusChange(status);
-    //console.log('A server status changed, new status: ' + status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../logging.service';
 import { AccountsService } from '../accounts.service';
 
@@ -9,17 +9,10 @@ import { AccountsService } from '../accounts.service';
   providers: [LoggingService]
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
   constructor(private loggingService: LoggingService, private accountsService: AccountsService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    // this.accountAdded.emit({
-    //   name: accountName,
-    //   status: accountStatus
-    // });
-    this.accountsService.addAccount(accountName, accountStatus);
-    this.loggingService.logStatusChange(accountStatus);
-    //console.log('A server status changed, new status: ' + accountStatus);
+    this.accountsService.addAccount(accountName, accountStatus);    
   }
 }
